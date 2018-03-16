6475364afbd3b58b4e7ae62beb6dfc27f1de1293
Bharath Kumar R <BharathKumarRavichandran@users.noreply.github.com>
2018-03-16 19:52:47 +0530

    JS error removed, audio files tested

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
-var canvas = document.querySelector("canvas");
+/*var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
-ctx.font = "9px Arial";
+ctx.font = "9px Arial";*/
 
+var dx =1; //Bird's velocity
 var i;
+var j;
+var p; //Tunnel's X-xoordinate
+var q; //Tunnel's Y-coordinate
+var score = 0;
 var x;  //Bird's X-coordinate
 var y;  //Bird's Y-coordinate
-var dx =1; //Bird's velocity
-var score = 0;
 
 var die = new Audio('audio/die.wav');
 var hit = new Audio('audio/hit.wav');
@@ -14,7 +17,8 @@ var point = new Audio('audio/point.wav');
 var swoosh = new Audio('audio/swoosh.wav');
 var wing = new Audio('audio/wing.wav');
 
-function stopAudio(audio) {    //Function to stop audio the current audio playing
+function stopAudio(audio) {    //Function to stop audio the current audio from playing
     audio.pause();
     audio.currentTime = 0;
 }
+
